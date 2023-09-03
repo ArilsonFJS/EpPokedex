a2803fb8a096925f22658cb10290d78ddc04834c
ArilsonFJS <arilsonsilva61@gmail.com>
2023-09-03 17:32:32 -0300

    subindo EP

--- eppokedexdb/spiders/pokemon.py
+++ eppokedexdb/spiders/pokemon.py
@@ -8,23 +8,18 @@ class PokemonSpider(scrapy.Spider):
     start_urls = ['https://pokemondb.net/pokedex/all']
 
     def parse(self, response):
-        # Encontre a tabela que contém os nomes dos Pokémon
         table = response.css('#pokedex')
 
-        # Iterar pelas linhas da tabela (exceto o cabeçalho)
         for row in table.css('tr')[1:]:
-            # Extrair o número do Pokémon da primeira coluna
             num = row.css('td:nth-child(1)::text').get()
 
-            # Extrair a URL da página do Pokémon
             pokemon_url = response.urljoin(
                 row.css('td:nth-child(2) a::attr(href)').get())
 
-            # Extrair o nome do Pokémon da segunda coluna
             name = row.css('td:nth-child(2) a::text').get()
 
-            # Extrair as próximas evoluções do Pokémon (PokéNum, nome e URL)
             evolution = row.css('td:nth-child(3) a')
+
             if evolution:
                 evolution_num = evolution.css('::text').get()
                 evolution_name = evolution.css('::attr(title)').get()
@@ -35,16 +30,12 @@ class PokemonSpider(scrapy.Spider):
                 evolution_name = None
                 evolution_url = None
 
-            # Extrair o tamanho do Pokémon
             height = row.css('td:nth-child(4)::text').get()
 
-            # Extrair o peso do Pokémon
             weight = row.css('td:nth-child(5)::text').get()
 
-            # Extrair os tipos do Pokémon
             types = row.css('td:nth-child(6) a::text').getall()
 
-            # Se o nome não estiver vazio, retornar todas as informações como um item JSON
             if name:
                 yield {
                     'num': num,
